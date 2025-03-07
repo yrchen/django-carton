07d6a7df027db90b3eddb3147a3edfc01f825f2f
Xaver Y.R. Chen <yrchen@ATCity.org>
2025-03-08 00:16:12 +0800

    fix: update setup.py to fix import issue

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from setuptools import setup, find_packages
+import os
+
+from distutils.core import setup
+from setuptools import find_packages
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 import carton
 
